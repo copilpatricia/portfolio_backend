b52560a43a47c6e8824eae39c6135f1bd65f9e2a
Patricia Muresan <patriciamariamrsn@gmail.com>
2024-03-13 19:57:57 -0400

    import express, morgan, cors and create an instance of the express calling the express function

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// middlewares
+app.use(cors()); // allows frontend to connect to backend
+app.use(morgan("dev")); //logger - info about the request
+app.use(express.json()); // for data in req.body
+app.use(express.urlencoded({extended: true})) // allow data in url string
