368d9b360b66ab49fb8b97f4f1cdd2d1a562ee5a
Patricia Muresan <patriciamariamrsn@gmail.com>
2024-03-13 20:30:18 -0400

    import the connection and log the message to test MongoDB

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+import './loadEnv.js';
+import {conn} from './db/conn.js';
+conn(); 
+
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 
+
 const app = express();
 const PORT = process.env.PORT || 4000;
+
 
 // middlewares
 app.use(cors()); // allows frontend to connect to backend
