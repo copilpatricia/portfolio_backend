9c4ab63eb38918ad6a732d1d460aee2314ad059e
Patricia Muresan <patriciamariamrsn@gmail.com>
2024-03-14 21:32:36 -0400

    create the projects schema

---
+++ models/projects.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const projectsSchema = new mongoose.Schema({
+    title: {
+        type: String,
+    },
+
+    description: {
+        type: String,
+        minLength: 10
+    },
+
+    image: {
+        type: String,
+    },
+
+    technologies: {
+        type: Array,
+        default: []
+    }
+});
+
+export default mongoose.model('Projects', projectsSchema)
