9f758d196d1a7abddb9757b9096e838fb647a6b0
Patricia Muresan <patriciamariamrsn@gmail.com>
2024-03-22 19:18:13 -0400

    create the post route

--- routes/projects.js
+++ routes/projects.js
@@ -25,3 +25,13 @@ router.get('/:id', async(req, res) => {
     }
 });
 
+// POST route - to create projects
+
+router.post('/', async(req, res) => {
+    try {
+        const project = await Projects.create(req.body);
+        res.status(200).json(project)
+    } catch (error) {
+        console.log(error)
+    }
+})
